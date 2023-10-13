ce13391a483cf090e4c8514a939c2039d63b9fbc
Mohammad Madjide <97970232+mohammadfaizalabdul@users.noreply.github.com>
2023-10-13 23:01:10 +0200

    Update script.js
    Mudei a mensagem Chabir para too high em numa das funções

--- Site_teste/Advinha_o_numero/script.js
+++ Site_teste/Advinha_o_numero/script.js
@@ -58,7 +58,7 @@ document.querySelector('.check').addEventListener('click', function(){
     } else if(guess !== secretNumber){
 
         if(score > 1){
-          displayMessage( guess > secretNumber ? ' Chabir' : 'Too low!')
+          displayMessage( guess > secretNumber ? ' Too high' : 'Too low!')
 
             score --
             document.querySelector('.score').textContent = score
